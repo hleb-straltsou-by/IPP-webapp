b5dfa9b6da26b6fef62b121ec82803979ba41068
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-11-08 23:11:22 +0300

    add researches.component

---
+++ client/src/app/researches/researches.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'researches',
+    templateUrl: './researches.component.html',
+    styleUrls: ['./researches.component.css']
+})
+
+export class ResearchesComponent {
+    header = "Direction of researches";
+}
