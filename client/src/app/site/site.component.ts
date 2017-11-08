2f43385c8bde8c4c831889d9f6ff0a94bc0f77f8
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-11-08 23:11:49 +0300

    add site.component

---
+++ client/src/app/site/site.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'site',
+    templateUrl: './site.component.html',
+    styleUrls: ['./site.component.css']
+})
+
+export class SiteComponent {
+    header = "Site";
+}
