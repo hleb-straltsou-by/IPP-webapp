34b2b361b1900c4436dcdbfa1320bd2c7b5a24cf
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-11-08 23:11:37 +0300

    add scientists.component

---
+++ client/src/app/scientists/scientists.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'scientists',
+    templateUrl: './scientists.component.html',
+    styleUrls: ['./scientists.component.css']
+})
+
+export class ScientistsComponent {
+    header = "Young Scientists League";
+}
