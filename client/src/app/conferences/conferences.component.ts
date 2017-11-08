9c41e1c4f1b83fbb144a0f00966805bf0f123b80
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-11-08 23:09:44 +0300

    add conferences.component

---
+++ client/src/app/conferences/conferences.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'conferences',
+    templateUrl: './conferences.component.html',
+    styleUrls: ['./conferences.component.css']
+})
+
+export class ConferencesComponent {
+    header = "Conferences";
+}
