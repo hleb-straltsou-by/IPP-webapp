3b92d7e35adff2f2f22a9080a9299dbf9886d0d9
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-11-08 23:10:59 +0300

    add products.component

---
+++ client/src/app/products/products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.css']
+})
+
+export class ProductsComponent {
+    header = "Products";
+}
