15c77a53649a0a1db527f018f1bd654b39c83341
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-11-08 23:12:02 +0300

    add society.component

---
+++ client/src/app/society/society.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'society',
+    templateUrl: './society.component.html',
+    styleUrls: ['./society.component.css']
+})
+
+export class SocietyComponent {
+    header = "Phytopathological society";
+}
