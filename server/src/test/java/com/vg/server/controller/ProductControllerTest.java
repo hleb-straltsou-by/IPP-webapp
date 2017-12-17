0bc7023f2d4ac43e213f967a8706deaecaa5c2f2
vtrusevich <gleb.streltsov.4by@gmail.com>
2017-12-17 15:44:09 +0300

    add tests for Product API

--- server/src/test/java/com/vg/server/controller/ProductControllerTest.java
+++ server/src/test/java/com/vg/server/controller/ProductControllerTest.java
@@ -24,108 +24,97 @@ public class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private static final String HERO_NAME_JSON_PATH = "$.name";
-    private static final String HERO_ARRAY_SIZE_JSON_PATH = "$";
-    private static final String EMPTY_HERO_NAME = "none";
-    private static final String HERO_ID_PARAM = "id";
-    private static final String HEROES_COUNT_PARAM = "count";
-    private static final String HERO_NAME_PARAM = "name";
+    private static final String PRODUCT_NAME_JSON_PATH = "$.name";
+    private static final String PRODUCT_ARRAY_SIZE_JSON_PATH = "$";
+    private static final String EMPTY_PRODUCT_NAME = "none";
+    private static final String PRODUCT_ID_PARAM = "id";
+    private static final String PRODUCT_COUNT_PARAM = "count";
+    private static final String PRODUCT_NAME_PARAM = "name";
 
-    private static final int DEFAULT_HEROES_LIMITED_COUNT = 10;
-    private static final int MAX_HEROES_LIMITED_COUNT = 16;
+    private static final int DEFAULT_PRODUCT_LIMITED_COUNT = 10;
+    private static final int MAX_PRODUCT_LIMITED_COUNT = 16;
 
     @Test
-    public void noParamHeroShouldReturnDefaultHero() throws Exception {
-        final String urlTemplate = "/api/hero";
+    public void noParamProductShouldReturnDefaultProduct() throws Exception {
+        final String urlTemplate = "/api/product";
 
         this.mockMvc.perform(get(urlTemplate))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath(HERO_NAME_JSON_PATH).value(EMPTY_HERO_NAME));
+                .andExpect(jsonPath(PRODUCT_NAME_JSON_PATH).value(EMPTY_PRODUCT_NAME));
     }
 
     @Test
-    public void paramHeroShouldReturnExistHero() throws Exception {
-        final String urlTemplate = "/api/hero";
+    public void validParamProductShouldReturnExistProduct() throws Exception {
+        final String urlTemplate = "/api/product";
         final String idParam = "11";
-        final String expectedHeroName = "Dynama";
+        final String expectedName = "Dynama";
 
-        this.mockMvc.perform(get(urlTemplate).param(HERO_ID_PARAM, idParam))
+        this.mockMvc.perform(get(urlTemplate).param(PRODUCT_ID_PARAM, idParam))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath(HERO_NAME_JSON_PATH).value(expectedHeroName));
+                .andExpect(jsonPath(PRODUCT_NAME_JSON_PATH).value(expectedName));
     }
 
     @Test
-    public void illegalParamHeroShouldReturnDefaultHero() throws Exception {
-        final String urlTemplate = "/api/hero";
+    public void illegalParamProductShouldReturnDefaultProduct() throws Exception {
+        final String urlTemplate = "/api/product";
         final String idParam = "illegal";
 
-        this.mockMvc.perform(get(urlTemplate).param(HERO_ID_PARAM, idParam))
+        this.mockMvc.perform(get(urlTemplate).param(PRODUCT_ID_PARAM, idParam))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath(HERO_NAME_JSON_PATH).value(EMPTY_HERO_NAME));
+                .andExpect(jsonPath(PRODUCT_NAME_JSON_PATH).value(EMPTY_PRODUCT_NAME));
     }
 
     @Test
-    public void paramHeroesShouldReturnCorrespondHeroesCount() throws Exception {
-        final String urlTemplate = "/api/heroes";
-        final String countParam = "7";
-        final int expectedHeroCount = 7;
-
-        this.mockMvc.perform(get(urlTemplate).param(HEROES_COUNT_PARAM, countParam))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath(HERO_ARRAY_SIZE_JSON_PATH, hasSize(expectedHeroCount)));
-    }
-
-    @Test
-    public void illegalParamHeroesShouldReturnDefaultHeroCount() throws Exception {
-        final String urlTemplate = "/api/heroes";
+    public void illegalParamProductShouldReturnDefaultProductCount() throws Exception {
+        final String urlTemplate = "/api/products";
         final String countParam = "-7";
-        final int expectedHeroCount = DEFAULT_HEROES_LIMITED_COUNT;
+        final int expectedCount = DEFAULT_PRODUCT_LIMITED_COUNT;
 
-        this.mockMvc.perform(get(urlTemplate).param(HEROES_COUNT_PARAM, countParam))
+        this.mockMvc.perform(get(urlTemplate).param(PRODUCT_COUNT_PARAM, countParam))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath(HERO_ARRAY_SIZE_JSON_PATH, hasSize(expectedHeroCount)));
+                .andExpect(jsonPath(PRODUCT_ARRAY_SIZE_JSON_PATH, hasSize(expectedCount)));
     }
 
     @Test
-    public void noParamHeroesShouldReturnAllHeroesCount() throws Exception {
-        final String urlTemplate = "/api/heroes";
-        final int expectedHeroCount = MAX_HEROES_LIMITED_COUNT;
+    public void noParamProductsShouldReturnAllProductsCount() throws Exception {
+        final String urlTemplate = "/api/products";
+        final int expectedCount = MAX_PRODUCT_LIMITED_COUNT;
 
         this.mockMvc.perform(get(urlTemplate))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath(HERO_ARRAY_SIZE_JSON_PATH, hasSize(expectedHeroCount)));
+                .andExpect(jsonPath(PRODUCT_ARRAY_SIZE_JSON_PATH, hasSize(expectedCount)));
     }
 
     @Test
-    public void addHeroShouldReturnCorrespondHeroWithCorrespondId() throws Exception {
-        final String urlTemplate = "/api/add/hero";
+    public void addProductShouldReturnCorrespondProductWithCorrespondId() throws Exception {
+        final String urlTemplate = "/api/add/product";
         final String expectedName = "testName";
         final int expectedId = 17;
 
-        String heroNameJson = "{ " + HERO_NAME_PARAM + " : \"" + expectedName + "\" }";
+        String nameJson = "{ " + PRODUCT_NAME_PARAM + " : \"" + expectedName + "\" }";
 
         this.mockMvc.perform(post(urlTemplate)
-                .content(heroNameJson))
+                .content(nameJson))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath(HERO_NAME_JSON_PATH).value(expectedName))
-                .andExpect(jsonPath(HERO_ID_PARAM).value(expectedId));
+                .andExpect(jsonPath(PRODUCT_NAME_JSON_PATH).value(expectedName))
+                .andExpect(jsonPath(PRODUCT_ID_PARAM).value(expectedId));
 
-        final String deleteUrlTemplate = "/api/delete/hero";
-        final String addedHeroId = "17";
-        this.mockMvc.perform(delete(deleteUrlTemplate).param(HERO_ID_PARAM, addedHeroId))
+        final String deleteUrlTemplate = "/api/delete/product";
+        final String addedProductId = "17";
+        this.mockMvc.perform(delete(deleteUrlTemplate).param(PRODUCT_ID_PARAM, addedProductId))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void findHeroesByNameShouldReturnCorrepondListOfHeroes() throws Exception {
-        final String urlTemplate = "/api/heroes/search/findByName";
+    public void findProductsByNameShouldReturnCorrespondListOfProducts() throws Exception {
+        final String urlTemplate = "/api/products/search/findByName";
         final String nameParam = "b";
-        final int expectedHeroesListSize = 5;
+        final int expectedListSize = 5;
 
-        this.mockMvc.perform(get(urlTemplate).param(HERO_NAME_PARAM, nameParam))
+        this.mockMvc.perform(get(urlTemplate).param(PRODUCT_NAME_PARAM, nameParam))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath(HERO_ARRAY_SIZE_JSON_PATH, hasSize(expectedHeroesListSize)));
+                .andExpect(jsonPath(PRODUCT_ARRAY_SIZE_JSON_PATH, hasSize(expectedListSize)));
     }
 }
